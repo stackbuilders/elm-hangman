edeaf64e5287feb5a880c69c0015ec9b1f46cae9
Juan Pedro Villa Isaza <jpvillaisaza@gmail.com>
2015-06-22 17:30:25 -0500

    Add example 1

---
+++ examples/1/Hangman.elm
@@ -0,0 +1,77 @@
+module Hangman where
+
+
+import Char
+import Html exposing (Html)
+import Html.Attributes
+import Html.Events
+import StartApp
+import String
+
+
+-- Model
+
+type alias Model =
+  List (Char, Bool)
+
+
+init : String -> Model
+init word =
+  String.foldr (::) [] word
+    |> List.map (\letter -> (letter, False))
+
+
+-- Update
+
+type alias Action =
+  Char
+
+
+update : Action -> Model -> Model
+update guess letters =
+  let
+    match guess (letter, guessed) =
+      (letter, guess == letter || guessed)
+  in
+    List.map (match guess) letters
+
+
+-- view
+
+view : Signal.Address Action -> Model -> Html
+view address model =
+  Html.div
+    [ Html.Attributes.style [ ("text-align", "center") ]
+
+    ]
+    [ Html.h1 [] [ Html.text "Hangman" ]
+    , Html.h3 [] [ Html.text (toString model) ]
+    , Html.input
+        [ Html.Attributes.autofocus True
+        , Html.Attributes.placeholder "guess"
+        , Html.Attributes.style [ ("text-align", "center") ]
+        , Html.Events.onKeyPress address Char.fromCode
+        ]
+        []
+    ]
+
+
+toString : Model -> String
+toString letters =
+  let
+    fromLetter (letter, guessed) =
+      if guessed then letter else '-'
+  in
+    List.map fromLetter letters
+      |> List.foldr String.cons ""
+
+
+--
+
+main : Signal Html
+main =
+  StartApp.start
+    { model = init "guayaquil"
+    , view = view
+    , update = update
+    }
